cd5229e5ba815414687525e5ce7c6f8838a56643
kobezzza <kobezzza@gmail.com>
2018-02-24 14:44:08 +0300

    Added net

---
+++ src/core/net.ts
@@ -0,0 +1,14 @@
+/*!
+ * V4Fire Core
+ * https://github.com/V4Fire/Core
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Core/blob/master/LICENSE
+ */
+
+/**
+ * If online returns true
+ */
+export async function isOnline(): Promise<boolean> {
+	return true;
+}
