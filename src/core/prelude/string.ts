ee0bc141253c1216d3a024d16b2264ee5ddfedae
kobezzza <kobezzza@gmail.com>
2018-11-23 03:46:42 +0300

    Removed Sugar

--- src/core/prelude/string.ts
+++ src/core/prelude/string.ts
@@ -8,73 +8,127 @@
 
 import extend from 'core/prelude/extend';
 
-try {
-	const
-		{camelize, dasherize, underscore} = String.prototype;
+const
+	camelizeCache = Object.createDict<string>(),
+	dasherizeCache = Object.createDict<string>(),
+	underscoreCache = Object.createDict<string>();
 
-	const
-		camelizeCache = Object.createDict<string>(),
-		dasherizeCache = Object.createDict<string>(),
-		underscoreCache = Object.createDict<string>();
+const
+	normalizeRgxp = /(?:^|([^\s_-]))[\s_-]+(?:$|([^\s_-]))/g;
 
+/** @see Sugar.String.camelize */
+extend(String.prototype, 'camelize', function (this: string, upper?: boolean): string {
 	const
-		needCamelize = /[\s_-]|[^\w$]/;
+		str = this.toString(),
+		val = camelizeCache[str];
 
-	/** @override */
-	String.prototype.camelize = function (this: string, upper?: boolean): string {
+	if (val !== undefined) {
+		return val;
+	}
+
+	let
+		res = str.toLowerCase().replace(normalizeRgxp, toCamelize);
+
+	if (upper) {
+		res = res[0].toUpperCase() + res.slice(1);
+	}
+
+	return camelizeCache[str] = res;
+});
+
+/** @see Sugar.String.dasherize */
+extend(String.prototype, 'dasherize', function (this: string): string {
+	const
+		str = this.toString(),
+		val = dasherizeCache[str];
+
+	if (val !== undefined) {
+		return val;
+	}
+
+	return dasherizeCache[str] = finalizeTpl(str.replace(normalizeRgxp, toDasherize), '-');
+});
+
+/** @see Sugar.String.underscore */
+extend(String.prototype, 'underscore', function (this: string): string {
+	const
+		str = this.toString(),
+		val = underscoreCache[str];
+
+	if (val !== undefined) {
+		return val;
+	}
+
+	return underscoreCache[str] = finalizeTpl(str.replace(normalizeRgxp, toUnderscore), '_');
+});
+
+function isUpper(char: string): boolean {
+	const up = char.toUpperCase();
+	return char === up && char.toLowerCase() !== up;
+}
+
+function toCamelize(str: string, $1?: string, $2?: string): string {
+	if ($1 && $2) {
+		return $1 + $2.toUpperCase();
+	}
+
+	return ($1 || '') + ($2 || '');
+}
+
+function toDasherize(str: string, $1?: string, $2?: string): string {
+	if ($1 && $2) {
+		return `${$1}-${$2}`;
+	}
+
+	return ($1 || '') + ($2 || '');
+}
+
+function toUnderscore(str: string, $1?: string, $2?: string): string {
+	if ($1 && $2) {
+		return `${$1}_${$2}`;
+	}
+
+	return ($1 || '') + ($2 || '');
+}
+
+function finalizeTpl(str: string, char: string): string {
+	let
+		res = '',
+		begin = true;
+
+	for (let i = 0; i < str.length; i++) {
 		const
-			str = this.toString(),
-			val = camelizeCache[str];
+			el = str[i];
 
-		if (val !== undefined) {
-			return val;
+		if (el === char) {
+			res += char;
+			continue;
 		}
 
-		if (needCamelize.test(str)) {
-			return camelizeCache[str] = camelize.call(str, upper);
+		if (res[res.length - 1] === char) {
+			res += el.toLowerCase();
+			continue;
 		}
 
-		return str;
-	};
-
-	const
-		needDasherize = /[A-Z\s_-]|[^\w$]/;
-
-	/** @override */
-	String.prototype.dasherize = function (this: string): string {
 		const
-			str = this.toString(),
-			val = dasherizeCache[str];
+			nextChar = str[i + 1];
 
-		if (val !== undefined) {
-			return val;
+		if (isUpper(el)) {
+			if (!begin && nextChar && !isUpper(nextChar)) {
+				res += char;
+			}
+
+			res += el.toLowerCase();
+
+		} else {
+			res += el;
+			begin = false;
+
+			if (nextChar && isUpper(nextChar)) {
+				res += char;
+			}
 		}
+	}
 
-		if (needDasherize.test(str)) {
-			return dasherizeCache[str] = dasherize.call(str);
-		}
-
-		return str;
-	};
-
-	const
-		needUnderscore = /[A-Z\s-]|[^\w$]/;
-
-	/** @override */
-	String.prototype.underscore = function (this: string): string {
-		const
-			str = this.toString(),
-			val = needUnderscore[str];
-
-		if (val !== undefined) {
-			return val;
-		}
-
-		if (needUnderscore.test(str)) {
-			return underscoreCache[str] = underscore.call(str);
-		}
-
-		return str;
-	};
-
-} catch {}
+	return res;
+}
