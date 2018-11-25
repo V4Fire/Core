306bc091adde834ab296c35ec1175a78e690a19a
kobezzza <kobezzza@gmail.com>
2018-11-25 14:33:04 +0300

    Improved extend function

--- src/core/prelude/extend.ts
+++ src/core/prelude/extend.ts
@@ -13,25 +13,29 @@
  * @param name - method name
  * @param method
  */
-export default function extend(obj: Function | object, name: string, method: Function): void {
+export default function extend(obj: Function | object, name: string, method: Function | PropertyDescriptor): void {
+	const descriptor = <PropertyDescriptor>{
+		configurable: true
+	};
+
+	if (typeof method === 'function') {
+		descriptor.writable = true;
+		descriptor.value = () => method;
+
+	} else {
+		Object.assign(descriptor, method);
+	}
+
+	//#if runtime has noGlobals
+
 	const
 		key = Symbol.for(`[[V4_PROP_TRAP:${name}]]`);
 
 	if (typeof obj === 'function') {
-		Object.defineProperty(obj, key, {
-			writable: true,
-			configurable: true,
-			value: () => method
-		});
+		Object.defineProperty(obj, key, descriptor);
 
 	} else {
-		Object.defineProperty(obj, key, {
-			writable: true,
-			configurable: true,
-			value(): unknown {
-				return method;
-			}
-		});
+		Object.defineProperty(obj, key, descriptor);
 
 		if (obj !== Object.prototype) {
 			Object.defineProperty(Object.prototype, key, {
@@ -43,4 +47,11 @@ export default function extend(obj: Function | object, name: string, method: Fun
 			});
 		}
 	}
+
+	//#endif
+	//#unless runtime has noGlobals
+
+	Object.defineProperty(obj, name, descriptor);
+
+	//#endunles
 }
