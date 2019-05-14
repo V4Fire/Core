08d8f617d933a8972e8e2a4c478cced4eb37c4f5
kobezzza <kobezzza@gmail.com>
2019-05-14 09:53:10 +0300

    Better doc

--- src/core/global.ts
+++ src/core/global.ts
@@ -9,7 +9,7 @@
 import { GLOBAL } from 'core/const/links';
 
 /**
- * Constructor for any types
+ * Converts the specified unknown value to any
  * @param obj
  */
 GLOBAL.Any = function Any(obj: unknown): any {
