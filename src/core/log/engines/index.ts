6c2257311cb4748ef404ff472c96a90a502b8a95
ExactlyNoSense <whois.ard@gmail.com>
2019-05-28 11:17:10 +0300

    Rename engine file

--- src/core/log/engines/index.ts
+++ src/core/log/engines/index.ts
@@ -6,7 +6,7 @@
  * https://github.com/V4Fire/Core/blob/master/LICENSE
  */
 
-import { log as console } from 'core/log/engines/console-log';
+import { log as console } from 'core/log/engines/console';
 export * from 'core/log/engines/types';
 
 const enginesStrategy = {
