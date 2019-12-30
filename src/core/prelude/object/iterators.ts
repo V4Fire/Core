61f8e3ae195652dab69d7b1be9e7d396f86cf4cf
kobezzza <kobezzza@gmail.com>
2019-12-30 14:46:55 +0300

    Improved doc

--- src/core/prelude/object/iterators.ts
+++ src/core/prelude/object/iterators.ts
@@ -14,9 +14,15 @@ const
 /**
  * Iterates over the specified object
  *
- * @param obj
- * @param cb
- * @param [opts]
+ * @param obj - object to iterate
+ * @param cb - callback function that is called on each of object elements
+ * @param [opts] - additional options:
+ *   *) [withDescriptor] - if true, then the first element of the callback function will be an element descriptor
+ *   *) [notOwn = false] - iteration type:
+ *
+ *     1) if false, then the hasOwnProperty test is enabled and all not own properties will be skipped;
+ *     2) if true, then the hasOwnProperty test is disabled;
+ *     3) if -1, then the hasOwnProperty test is enabled and all own properties will be skipped.
  */
 extend(Object, 'forEach', (obj: any, cb: Function, opts?: ObjectForEachOptions) => {
 	if (!obj) {
