def756e48936239c20029e94f3af03d520e8108a
kobezzza <kobezzza@gmail.com>
2020-01-10 23:48:34 +0300

    Improved doc

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,72 +6,29 @@
  * https://github.com/V4Fire/Core/blob/master/LICENSE
  */
 
+/**
+ * [[include:config/README.md]]
+ * @packageDocumentation
+ */
+
 import { Config } from 'config/interface';
 export * from 'config/interface';
 
-const config: Config = {
-	/**
-	 * The base application name
-	 */
+const config = <Config>{
 	appName: APP_NAME,
-
-	/**
-	 * Default system language
-	 * (used for internalizing)
-	 */
 	locale: LOCALE,
-
-	/**
-	 * Base API URL: the main domain of a service
-	 */
 	api: API_URL,
 
-	/**
-	 * Options for the online checking module
-	 */
 	online: {
-		/**
-		 * The URL which is used for online checking
-		 */
 		checkURL: '',
-
-		/**
-		 * A value in milliseconds after which the online check will be repeated
-		 */
 		checkInterval: (5).seconds(),
-
-		/**
-		 * A value that represents how long the last cache check is relevant
-		 * (in milliseconds)
-		 */
 		cacheTTL: 0.3.second(),
-
-		/**
-		 * A timeout value for the single online check
-		 * (in milliseconds)
-		 */
 		checkTimeout: (2).seconds(),
-
-		/**
-		 * The number of acceptable retries (if the check was canceled by a timeout)
-		 */
 		retryCount: 3,
-
-		/**
-		 * If true, then the checking results will be periodically saved to a storage.
-		 * It can be helpful for realizing how long an application lives without connection.
-		 */
 		persistence: true,
-
-		/**
-		 * A value in milliseconds after which there will be a synchronization of the last online date to the current date
-		 */
 		lastDateSyncInterval: (1).minute()
 	},
 
-	/**
-	 * Options for the logging module
-	 */
 	log: {
 		pipelines: [{
 				middlewares: ['configurable'],
--- src/config/interface.ts
+++ src/config/interface.ts
@@ -9,17 +9,67 @@
 import { LogConfig } from 'core/log';
 
 export interface Config {
+	/**
+	 * Base application name
+	 */
 	appName?: CanUndef<string>;
-	api?: CanUndef<string>;
+
+	/**
+	 * Default system language
+	 * (used for internalizing)
+	 */
 	locale: string;
+
+	/**
+	 * Base API URL: the main domain of a service
+	 */
+	api?: CanUndef<string>;
+
+	/**
+	 * Options for the logging module
+	 */
 	log: LogConfig;
+
+	/**
+	 * Options for the online checking module
+	 */
 	online: {
+		/**
+		 * URL which is used for online checking
+		 */
 		checkURL?: CanUndef<string>;
+
+		/**
+		 * Value in milliseconds after which the online check will be repeated
+		 */
 		checkInterval?: number;
+
+		/**
+		 * Value that represents how long the last cache check is relevant
+		 * (in milliseconds)
+		 */
 		checkTimeout?: number;
+
+		/**
+		 * Timeout value for the single online check
+		 * (in milliseconds)
+		 */
 		cacheTTL?: number;
+
+		/**
+		 * Number of acceptable retries (if the check was canceled by a timeout)
+		 */
 		lastDateSyncInterval?: number;
+
+		/**
+		 * If true, then the checking results will be periodically saved to a storage.
+		 * It can be helpful for realizing how long an application lives without connection.
+		 */
 		retryCount?: number;
+
+		/**
+		 * Value in milliseconds after which there will be a synchronization of the last online date to the current date
+		 */
 		persistence?: boolean;
 	}
 }
