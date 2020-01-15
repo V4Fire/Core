462104de68ba847c3f436347402e7baee54b7447
kobezzza <kobezzza@gmail.com>
2020-01-15 15:23:50 +0300

    Improved docs and API

--- src/core/event/index.ts
+++ src/core/event/index.ts
@@ -12,16 +12,27 @@
  */
 
 import SyncPromise from 'core/promise/sync';
+import Async, { EventEmitterLike } from 'core/async';
 import { deprecate } from 'core/meta/deprecation';
-import { EventEmitter2 as EventEmitter } from 'eventemitter2';
 
 /**
  * Returns a promise that will be resolved after emitting of all events from the specified emitter
  *
  * @param emitter
- * @param events
+ * @param events - events to listen
+ *
+ * @example
+ * ```js
+ * // The promise will be resolved after two window events
+ * resolveAfterEvents(window, 'resize', 'scroll').then(() => {
+ *   console.log('Boom!');
+ * });
+ * ```
  */
-export function resolveAfterEvents(emitter: EventEmitter, ...events: string[]): SyncPromise<void> {
+export function resolveAfterEvents(emitter: EventEmitterLike, ...events: string[]): SyncPromise<void> {
+	const
+		$a = new Async();
+
 	return new SyncPromise((resolve) => {
 		const
 			res = {};
@@ -31,7 +42,7 @@ export function resolveAfterEvents(emitter: EventEmitter, ...events: string[]): 
 				ev = events[i];
 
 			res[ev] = false;
-			emitter.once(ev, () => {
+			$a.once(emitter, ev, () => {
 				res[ev] = true;
 
 				if (events.every((e: string) => res[e])) {
@@ -60,9 +71,23 @@ export function resolveAfterDOMLoaded(): SyncPromise<void> {
  * Wraps a callback function into a new function that never calls the target until all specified flags are resolved.
  * The function returns a new function that takes a string flag and resolves it.
  * After all flags are resolved, the last function invokes the target function.
+ * If you try to invoke the function after the first time resolving, ii won't be executed.
  *
- * @param cb
- * @param flags
+ * @param cb - callback function that is invoked after resolving all flags
+ * @param flags - flags to resolve
+ *
+ * @example
+ * ```js
+ * const semaphore = createsAsyncSemaphore(() => {
+ *   console.log('Boom!');
+ * }, 'foo', 'bar');
+ *
+ * semaphore('foo');
+ * semaphore('bar'); // 'Boom!'
+ *
+ * // Function already resolved, the target function isn't executed
+ * semaphore();
+ * ```
  */
 export function createsAsyncSemaphore<T>(cb: () => T, ...flags: string[]): (flag: string) => CanUndef<T> {
 	const
@@ -120,7 +145,7 @@ export const afterEvents = deprecate(
 		alternative: 'resolveAfterEvents'
 	},
 
-	function afterEvents(emitter: EventEmitter, cb: Function | string, ...events: string[]): SyncPromise<void> {
+	function afterEvents(emitter: EventEmitterLike, cb: Function | string, ...events: string[]): SyncPromise<void> {
 		const
 			promise = resolveAfterEvents(emitter, ...(<string[]>[]).concat(Object.isString(cb) ? cb : [], events));
 
