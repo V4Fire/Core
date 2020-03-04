df328669d5f5a890c5059ecd42820b21ee082f95
kobezzza <kobezzza@gmail.com>
2020-03-04 20:01:24 +0300

    Added index for core/object

---
+++ src/core/object/index.ts
@@ -0,0 +1,15 @@
+/*!
+ * V4Fire Core
+ * https://github.com/V4Fire/Core
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Core/blob/master/LICENSE
+ */
+
+/**
+ * [[include:core/object/README.md]]
+ * @packageDocumentation
+ */
+
+export { default as select } from 'core/object/select';
+export { watch, set, unset } from 'core/object/watch';
