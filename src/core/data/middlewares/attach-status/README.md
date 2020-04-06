3c6899bbff8d7253cab930b11913475e23dea3df
Andrey Kobets <kobezzza@gmail.com>
2020-04-06 21:14:23 +0300

    Added attachStatus (#73)
    * Added attachStatus
    
    * :up:

---
+++ src/core/data/middlewares/attach-status/README.md
@@ -0,0 +1,3 @@
+# core/data/middlewares/attach-status
+
+This module provides a decoder middleware to attach a response status to response data.
---
+++ src/core/data/middlewares/attach-status/index.ts
@@ -0,0 +1,30 @@
+/*!
+ * V4Fire Core
+ * https://github.com/V4Fire/Core
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Core/blob/master/LICENSE
+ */
+
+/**
+ * [[include:core/data/middlewares/attach-status/README.md]]
+ * @packageDocumentation
+ */
+
+import { Response, MiddlewareParams } from 'core/request';
+import { DataWithStatus } from 'core/data/middlewares/attach-status/interface';
+export * from 'core/data/middlewares/attach-status/interface';
+
+/**
+ * Decoder: attaches a response status to response data
+ *
+ * @param data
+ * @param params
+ * @param response
+ */
+export function attachStatus<D>(data: D, params: MiddlewareParams<D>, response: Response<D>): DataWithStatus<D> {
+	return {
+		data,
+		status: response.status
+	};
+}
