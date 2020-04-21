feadab2e5edc3c59bce9ae422c6aa07d5582b5ba
kobezzza <kobezzza@gmail.com>
2020-04-21 08:45:27 +0300

    Refactoring

---
+++ src/core/functools/lazy.ts
@@ -0,0 +1,61 @@
+/*!
+ * V4Fire Core
+ * https://github.com/V4Fire/Core
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Core/blob/master/LICENSE
+ */
+
+/**
+ * Decorator for Function.prototype.debounce
+ *
+ * @decorator
+ * @see [[Function.debounce]]
+ * @param [delay] - delay value (in milliseconds)
+ */
+export function debounce(delay?: number): MethodDecorator {
+	return (target, key, descriptor: PropertyDescriptor) => {
+		const
+			method = descriptor.value;
+
+		if (!Object.isFunction(method)) {
+			throw new TypeError(`descriptor.value is not a function: ${method}`);
+		}
+
+		descriptor.value = function (): unknown {
+			Object.defineProperty(this, key, {
+				configurable: true,
+				value: method.debounce(delay)
+			});
+
+			return this[key].apply(this, arguments);
+		};
+	};
+}
+
+/**
+ * Decorator for Function.prototype.throttle
+ *
+ * @decorator
+ * @see [[Function.throttle]]
+ * @param [delay] - delay value (in milliseconds)
+ */
+export function throttle(delay?: number): MethodDecorator {
+	return (target, key, descriptor: PropertyDescriptor) => {
+		const
+			method = descriptor.value;
+
+		if (!Object.isFunction(method)) {
+			throw new TypeError(`descriptor.value is not a function: ${method}`);
+		}
+
+		descriptor.value = function (): unknown {
+			Object.defineProperty(this, key, {
+				configurable: true,
+				value: method.throttle(delay)
+			});
+
+			return this[key].apply(this, arguments);
+		};
+	};
+}
---
+++ src/core/functools/memoize.ts
@@ -0,0 +1,31 @@
+/*!
+ * V4Fire Core
+ * https://github.com/V4Fire/Core
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Core/blob/master/LICENSE
+ */
+
+/**
+ * Decorator for Function.prototype.once
+ *
+ * @decorator
+ * @see [[Function.once]]
+ */
+export function once(target: Object, key: string | symbol, descriptor: PropertyDescriptor): void {
+	const
+		method = descriptor.value;
+
+	if (!Object.isFunction(method)) {
+		throw new TypeError(`descriptor.value is not a function: ${method}`);
+	}
+
+	descriptor.value = function (): unknown {
+		Object.defineProperty(this, key, {
+			configurable: true,
+			value: method.once()
+		});
+
+		return this[key].apply(this, arguments);
+	};
+}
