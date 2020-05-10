561b64e90bfa5b93cdec9a8e53fcd0a2762e3ecc
kobezzza <kobezzza@gmail.com>
2020-05-10 14:41:54 +0300

    Review spec

--- src/core/async/modules/proxy/promise.spec.js
+++ src/core/async/modules/proxy/promise.spec.js
@@ -12,28 +12,29 @@ describe('core/async/modules/proxy/promise', () => {
 	const $a = new Async();
 
 	it('simple promise wrapper with the resolved promise', async () => {
-		await $a.promise(Promise.resolve('All fine')).then((msg) => expect(msg).toBe('All fine'));
+		expect(await $a.promise(Promise.resolve('All fine'))).toBe('All fine');
 	});
 
 	it('chained promise wrapper with the resolved promise', async () => {
-		await $a.promise(Promise.resolve('All fine'))
-			.catch(() => 'Boom!')
-			.then((msg) => expect(msg).toBe('All fine'));
+		expect(await $a.promise(Promise.resolve('All fine')).catch(() => 'Boom!')).toBe('All fine');
 	});
 
 	it('simple promise wrapper with the rejected promise', async () => {
-		await $a.promise(Promise.reject('Boom!')).catch((err) => {
+		try {
+			await $a.promise(Promise.reject('Boom!'));
+			throw 'Oops';
+
+		} catch (err) {
 			expect(err).toBe('Boom!');
-		});
+		}
 	});
 
 	it('chained promise wrapper with the rejected promise', async () => {
-		await $a.promise(Promise.reject('Boom!'))
-			.catch((err) => {
-				expect(err).toBe('Boom!');
-				return 'All fine';
-			})
+		const res = $a.promise(Promise.reject('Boom!')).catch((err) => {
+			expect(err).toBe('Boom!');
+			return 'All fine';
+		});
 
-			.then((msg) => expect(msg).toBe('All fine'));
+		expect(await res).toBe('All fine');
 	});
 });
