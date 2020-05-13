06dfccb9b4843c3b7ea7a4509881b183db7902bd
kobezzza <kobezzza@gmail.com>
2020-05-13 09:54:30 +0300

    Fixed invalid compile options

--- config/default.js
+++ config/default.js
@@ -320,7 +320,7 @@ module.exports = config.createConfig(
 		typescript() {
 			const
 				es = this.es(),
-				importHelpers = Boolean({ES3: true, ES5: true, ES6: true})[es];
+				importHelpers = Boolean({ES3: true, ES5: true, ES6: true}[es]);
 
 			return {
 				transpileOnly: true,
