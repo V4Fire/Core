991cac0370e41c02bc72777b123e7fd3012ab54f
kobezzza <kobezzza@gmail.com>
2020-06-17 09:39:13 +0300

    Added tests

---
+++ src/core/prelude/structures/sync-promise/spec.js
@@ -0,0 +1,214 @@
+/*!
+ * V4Fire Core
+ * https://github.com/V4Fire/Core
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Core/blob/master/LICENSE
+ */
+
+import SyncPromise from 'core/promise/sync';
+
+describe('core/prelude/structures/sync-promise', () => {
+	it('simple then', () => {
+		let
+			i = 1;
+
+		new SyncPromise((resolve) => {
+			resolve();
+		})
+			.then(() => i + 2)
+			.then((val) => i = val * 2);
+
+		expect(i).toBe(6);
+	});
+
+	it('rejected then', () => {
+		let
+			i = 1;
+
+		new SyncPromise((resolve, reject) => {
+			reject('boom');
+		})
+			.then((val) => val * 2, (err) => {
+				expect(err).toBe('boom');
+				i += 2;
+			});
+
+		expect(i).toBe(3);
+	});
+
+	it('dynamically rejected catch', () => {
+		let
+			i = 1;
+
+		new SyncPromise((resolve) => {
+			resolve();
+		})
+			.then(() => {
+				throw 'boom';
+			})
+
+			.then((val) => val * 2)
+			.catch((err) => {
+				expect(err).toBe('boom');
+				return i += 2;
+			})
+
+			.then((val) => i = val * 2);
+
+		expect(i).toBe(6);
+	});
+
+	it('catch', () => {
+		let
+			i = 1;
+
+		new SyncPromise((resolve, reject) => {
+			reject('boom');
+		})
+			.then((val) => val * 2)
+			.catch((err) => {
+				expect(err).toBe('boom');
+				i += 2;
+			});
+
+		expect(i).toBe(3);
+	});
+
+	it('dynamically rejected catch', () => {
+		let
+			i = 1;
+
+		new SyncPromise((resolve) => {
+			resolve();
+		})
+			.then(() => {
+				throw 'boom';
+			})
+
+			.then((val) => val * 2)
+			.catch((err) => {
+				expect(err).toBe('boom');
+				return i += 2;
+			})
+
+			.then((val) => i = val * 2);
+
+		expect(i).toBe(6);
+	});
+
+	it('resolved finally', () => {
+		let
+			i = 1;
+
+		new SyncPromise((resolve) => {
+			resolve();
+		})
+			.then(() => i + 2)
+			.finally((arg) => {
+				expect(arg).toBeUndefined();
+				i *= 2;
+			});
+
+		expect(i).toBe(2);
+	});
+
+	it('rejected finally', () => {
+		let
+			i = 1;
+
+		new SyncPromise((resolve, reject) => {
+			reject('boom');
+		})
+			.finally((arg) => {
+				expect(arg).toBeUndefined();
+				i *= 2;
+			});
+
+		expect(i).toBe(2);
+	});
+
+	it('dynamically rejected finally', () => {
+		let
+			i = 1;
+
+		new SyncPromise((resolve) => {
+			resolve();
+		})
+			.then(() => {
+				throw 'boom';
+			})
+
+			.then((val) => val * 2)
+
+			.catch((err) => {
+				expect(err).toBe('boom');
+				return i + 2;
+			})
+
+			.finally((arg) => {
+				expect(arg).toBeUndefined();
+				return i *= 2;
+			})
+
+			.then((val) => {
+				expect(val).toBe(3);
+				return i = val * 2;
+			});
+
+		expect(i).toBe(6);
+	});
+
+	it('static resolve', () => {
+		let
+			i = 0,
+			j = 0;
+
+		SyncPromise.resolve(1)
+			.then((val) => i = val + 2)
+			.then((val) => i = val * 2);
+
+		SyncPromise.resolve(SyncPromise.resolve(1))
+			.then((val) => j = val + 2)
+			.then((val) => j = val * 2);
+
+		expect(i).toBe(6);
+		expect(j).toBe(6);
+	});
+
+	it('static reject', () => {
+		let
+			i = 1;
+
+		SyncPromise.reject('boom')
+			.catch((err) => {
+				expect(err).toBe('boom');
+				i += 2;
+			});
+
+		expect(i).toBe(3);
+	});
+
+	it('static all', () => {
+		let res;
+
+		SyncPromise.all([
+			1,
+			null,
+			SyncPromise.resolve(2)
+		]).then((val) => res = val);
+
+		expect(res).toEqual([1, null, 2]);
+	});
+
+	it('static race', () => {
+		let res;
+
+		SyncPromise.race([
+			Promise.resolve(1),
+			SyncPromise.resolve(2)
+		]).then((val) => res = val);
+
+		expect(res).toBe(2);
+	});
+});
