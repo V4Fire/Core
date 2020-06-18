5c9822d5ca14a3106c693271a716e14328f5cca5
kobezzza <kobezzza@gmail.com>
2020-06-18 13:54:15 +0300

    Improved doc

--- index.d.ts
+++ index.d.ts
@@ -616,8 +616,32 @@ interface ObjectConstructor {
 	set<T>(obj: T, opts?: ObjectSetOptions, value?: unknown): (path: ObjectPropertyPath, value?: unknown) => T;
 
 	/**
-	 * Returns length of the specified object
+	 * Returns size/length of the specified object
+	 *
 	 * @param obj
+	 *
+	 * @example
+	 * ```js
+	 * // 1
+	 * Object.size({a: 1});
+	 *
+	 * // 2
+	 * Object.size([1, 2]);
+	 *
+	 * // 2
+	 * Object.size(new Set([1, 2]));
+	 *
+	 * // 2
+	 * Object.size((a, b) => a + b));
+	 *
+	 * // 1
+	 * Object.size(1);
+	 *
+	 * // 0
+	 * Object.size(NaN);
+	 * Object.size(null);
+	 * Object.size(undefined);
+	 * ```
 	 */
 	size(obj: unknown): number;
 
