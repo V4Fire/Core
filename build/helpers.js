ff453a84ad4321e8691820b6bee0a74d2a58c47d
kobezzza <kobezzza@gmail.com>
2020-06-29 09:55:10 +0300

    Added redefineRequire

--- build/helpers.js
+++ build/helpers.js
@@ -30,3 +30,54 @@ exports.getHead = function getHead(withVersion) {
 		.readFileSync(path.join(process.cwd(), 'disclaimer.txt'), 'utf-8')
 		.replace(/\* (\w.*?)(?=\n)/, (str) => str + (withVersion ? ` v${exports.getProjectInfo().version}` : ''));
 };
+
+/**
+ * Redefines global require functions to add support of WebPack layers
+ */
+exports.redefineRequire = function redefineRequire() {
+	const
+		{config: pzlr, resolve} = require('@pzlr/build-core');
+
+	const
+		staticRequire = require,
+		cache = Object.create(null);
+
+	const
+		outputDest = require('config').src.serverOutput(),
+		lib = path.join(outputDest, 'node_modules'),
+		deps = pzlr.dependencies;
+
+	// eslint-disable-next-line no-global-assign
+	require = (url) => {
+		if (url in cache) {
+			return staticRequire(cache[url]);
+		}
+
+		if (resolve.isNodeModule(url)) {
+			let
+				resolveUrl;
+
+			for (let i = 0; i < deps.length + 1; i++) {
+				try {
+					if (i) {
+						resolveUrl = staticRequire.resolve(path.join(lib, deps[i - 1], url));
+
+					} else {
+						resolveUrl = staticRequire.resolve(path.join(outputDest, url));
+					}
+
+					break;
+
+				} catch {}
+			}
+
+			if (resolveUrl) {
+				cache[url] = `./${path.relative(__dirname, resolveUrl)}`;
+				return staticRequire(cache[url]);
+			}
+		}
+
+		cache[url] = url;
+		return staticRequire(url);
+	};
+};
