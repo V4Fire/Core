21599cc9f69f896c91df546112fe01f8cab69415
kobezzza <kobezzza@gmail.com>
2020-06-29 09:19:56 +0300

    Added depsRgxpStr

---
+++ build/const.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/*!
+ * V4Fire Core
+ * https://github.com/V4Fire/Core
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Core/blob/master/LICENSE
+ */
+
+const
+	{config: {dependencies}} = require('@pzlr/build-core');
+
+/**
+ * String with project dependencies to use with regular expressions
+ */
+exports.depsRgxpStr = dependencies.map((el) => {
+	const src = Object.isString(el) ? el : el.src;
+	return src.split(/[\\/]/).map(RegExp.escape).join('[\\\\/]');
+}).join('|');
