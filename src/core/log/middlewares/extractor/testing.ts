9e325a10d4edebc9d8215cd201d315352c2dcc1d
ExactlyNoSense <whois.ard@gmail.com>
2021-03-15 01:31:52 +0300

    Tests for `extractor` middleware

---
+++ src/core/log/middlewares/extractor/testing.ts
@@ -0,0 +1,39 @@
+/*!
+ * V4Fire Core
+ * https://github.com/V4Fire/Core
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Core/blob/master/LICENSE
+ */
+
+import type { ErrorDetailsExtractor, ErrorCtor } from 'core/error';
+// tslint:disable-next-line:no-duplicate-imports
+import { BaseError } from 'core/error';
+
+export class TestDetailedError extends Error {
+	readonly reason: unknown;
+
+	constructor(message: string, reason: unknown) {
+		super(message);
+		this.reason = reason;
+	}
+}
+
+export class TestBaseError extends BaseError {}
+
+export class TestDetailedBaseError extends BaseError {
+	readonly reason: unknown;
+
+	constructor(message: string, reason: unknown, cause: Error) {
+		super(message, cause);
+		this.reason = reason;
+	}
+}
+
+export class TestExtractor implements ErrorDetailsExtractor<TestDetailedBaseError> {
+	target: ErrorCtor<TestDetailedBaseError> = TestDetailedBaseError;
+
+	extract(error: TestDetailedBaseError): unknown {
+		return error.reason;
+	}
+}
