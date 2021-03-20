dc171c5e810d5c5464850fca3633d162a3817f6b
ExactlyNoSense <whois.ard@gmail.com>
2021-03-20 22:23:14 +0300

    :white_check_mark: Add tests for BaseError

---
+++ src/core/error/testing.ts
@@ -0,0 +1,11 @@
+/*!
+ * V4Fire Core
+ * https://github.com/V4Fire/Core
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Core/blob/master/LICENSE
+ */
+
+import BaseError from 'core/error';
+
+export class TestError extends BaseError { }
