24ff7a0fc6fd0e9e1c3db0815bb66c5f8d0577d7
ExactlyNoSense <whois.ard@gmail.com>
2021-03-28 21:10:23 +0300

    Configurable middleware index

---
+++ src/core/log/middlewares/configurable/index.ts
@@ -0,0 +1,10 @@
+/*!
+ * V4Fire Core
+ * https://github.com/V4Fire/Core
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Core/blob/master/LICENSE
+ */
+
+export { ConfigurableMiddleware } from 'core/log/middlewares/configurable/configurable';
+export * from 'core/log/middlewares/configurable/interface';
