ec5a420f908ba4b707ceed13e57dd342c037e5b5
Razdva122 <lexur-02@yandex.ru>
2021-03-29 18:20:27 +0500

    interface docs

--- src/core/cache/decorators/persistent/engines/interface.ts
+++ src/core/cache/decorators/persistent/engines/interface.ts
@@ -9,20 +9,60 @@
 import type Cache from 'core/cache/interface';
 
 abstract class AbstractPersistentEngine<V = unknown> {
+	/**
+	 * Init cache before return instance of the cache
+	 * @param cache
+	 */
 	abstract initCache?(cache: Cache<V>): CanPromise<void>;
+
+	/**
+	 * Checking TTL of some property
+	 * @param key
+	 */
 	abstract getTTL(key: string): CanPromise<number | undefined>;
+
+	/**
+	 * Set value to the storage
+	 * @param key
+	 * @param value
+	 * @param ttl
+	 */
 	abstract set(key: string, value: V, ttl?: number): CanPromise<void>;
+
+	/**
+	 * Remove value from storage
+	 * @param key
+	 */
 	abstract remove(key: string): CanPromise<void>;
 }
 
 export abstract class AvailableToCheckInStorageEngine<V = unknown> extends AbstractPersistentEngine<V> {
+	/**
+	 * Get value from storage used only in action `check in storage`
+	 * @param key
+	 */
 	abstract get<T = unknown>(key: string): CanPromise<T | undefined>;
+
+	/**
+	 * Before every method 'get' | 'has' called this method is invoked
+	 * If method return true will call action `check in storage`
+	 *
+	 * @param method
+	 * @param key
+	 */
 	abstract isNeedToCheckInStorage(method: 'get' | 'has', key: string): CanPromise<boolean>;
 }
 
+/**
+ * Subtype of engine where `isNeedToCheckInStorage` always return false
+ * allows you not to implement method 'get'
+ */
 export abstract class UnavailableToCheckInStorageEngine<V = unknown> extends AbstractPersistentEngine<V> {
 	abstract isNeedToCheckInStorage(method: 'get' | 'has', key: string): CanPromise<false>;
 }
 
+/**
+ * Engine to connect logic with storage
+ */
 export type PersistentEngine<V = unknown> = AvailableToCheckInStorageEngine<V> | UnavailableToCheckInStorageEngine<V>;
 
