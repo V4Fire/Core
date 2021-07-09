d40159b5e60a99ad6e13183fbbd88b124c371981
kobezzza <kobezzza@gmail.com>
2021-07-09 10:33:28 +0300

    :art:

--- src/core/prelude/function/lazy/spec.js
+++ src/core/prelude/function/lazy/spec.js
@@ -106,6 +106,48 @@ describe('core/prelude/function/lazy', () => {
 		}, 20);
 	});
 
+	it('`throttle` with an interval', (done) => {
+		let i = 0;
+
+		const foo = (() => {
+			i++;
+		}).throttle({delay: 500});
+
+		const timerId = setInterval(foo, 10);
+
+		setTimeout(() => {
+			expect(i).toBe(3);
+			clearInterval(timerId);
+
+			setTimeout(() => {
+				expect(i).toBe(4);
+				done();
+			}, 500);
+
+		}, 1100);
+	});
+
+	it('`throttle` with an interval and skipping the rest calls', (done) => {
+		let i = 0;
+
+		const foo = (() => {
+			i++;
+		}).throttle({delay: 500, single: true});
+
+		const timerId = setInterval(foo, 10);
+
+		setTimeout(() => {
+			expect(i).toBe(3);
+			clearInterval(timerId);
+
+			setTimeout(() => {
+				expect(i).toBe(3);
+				done();
+			}, 500);
+
+		}, 1100);
+	});
+
 	it('`Function.throttle`', (done) => {
 		let i = 0;
 
@@ -167,47 +209,5 @@ describe('core/prelude/function/lazy', () => {
 			expect(i).toBe(1);
 			done();
 		}, 20);
-	});
-
-	it('`throttle` with interval', (done) => {
-		let i = 0;
-
-		const foo = (() => {
-			i++;
-		}).throttle({delay: 500});
-
-		const timerId = setInterval(foo, 10);
-
-		setTimeout(() => {
-			expect(i).toBe(3);
-			clearInterval(timerId);
-
-			setTimeout(() => {
-				expect(i).toBe(4);
-				done();
-			}, 500);
-
-		}, 1100);
-	});
-
-	it('`throttle` with interval with skipping the rest calls', (done) => {
-		let i = 0;
-
-		const foo = (() => {
-			i++;
-		}).throttle({delay: 500, single: true});
-
-		const timerId = setInterval(foo, 10);
-
-		setTimeout(() => {
-			expect(i).toBe(3);
-			clearInterval(timerId);
-
-			setTimeout(() => {
-				expect(i).toBe(3);
-				done();
-			}, 500);
-
-		}, 1100);
 	});
 });
