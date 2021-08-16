3036718b2001d8a092bf121d08f5f0d4e048a5d7
ExactlyNoSense <whois.ard@gmail.com>
2021-08-16 08:51:20 +0300

    Wrap existing tests with 'describe'

--- src/core/log/middlewares/errors-deduplicator/spec.js
+++ src/core/log/middlewares/errors-deduplicator/spec.js
@@ -9,91 +9,93 @@
 import { ErrorsDeduplicatorMiddleware } from 'core/log/middlewares/errors-deduplicator';
 
 describe('middlewares/errors-deduplicator', () => {
-	beforeEach(() => {
-		this.middleware = new ErrorsDeduplicatorMiddleware();
-	});
+	describe('implementation', () => {
+		beforeEach(() => {
+			this.middleware = new ErrorsDeduplicatorMiddleware();
+		});
 
-	afterEach(() => {
-		this.middleware = undefined;
-	});
+		afterEach(() => {
+			this.middleware = undefined;
+		});
 
-	it('an event without an error - the event should pass to the next middleware', () => {
-		const
-			event = createLogEvent(),
-			eventCopy = copyLogEvent(event),
-			nextCallbackSpy = jasmine.createSpy('next');
+		it('an event without an error - the event should pass to the next middleware', () => {
+			const
+				event = createLogEvent(),
+				eventCopy = copyLogEvent(event),
+				nextCallbackSpy = jasmine.createSpy('next');
 
-		this.middleware.exec(event, nextCallbackSpy);
-		expect(nextCallbackSpy).toHaveBeenCalledOnceWith(eventCopy);
-	});
+			this.middleware.exec(event, nextCallbackSpy);
+			expect(nextCallbackSpy).toHaveBeenCalledOnceWith(eventCopy);
+		});
 
-	it('single error occurrence - the event should pass to the next middleware', () => {
-		const
-			event = createLogEvent(new Error('My awesome error')),
-			eventCopy = copyLogEvent(event),
-			nextCallbackSpy = jasmine.createSpy('next');
+		it('single error occurrence - the event should pass to the next middleware', () => {
+			const
+				event = createLogEvent(new Error('My awesome error')),
+				eventCopy = copyLogEvent(event),
+				nextCallbackSpy = jasmine.createSpy('next');
 
-		this.middleware.exec(event, nextCallbackSpy);
-		expect(nextCallbackSpy).toHaveBeenCalledOnceWith(eventCopy);
-	});
+			this.middleware.exec(event, nextCallbackSpy);
+			expect(nextCallbackSpy).toHaveBeenCalledOnceWith(eventCopy);
+		});
 
-	it('multiple error occurrences - only the first event should pass to the next middleware', () => {
-		const
-			error = new Error('My awesome error'),
-			firstEvent = createLogEvent(error, 'first'),
-			firstEventCopy = copyLogEvent(firstEvent),
-			secondEvent = createLogEvent(error, 'second'),
-			firstNextCallbackSpy = jasmine.createSpy('firstNext'),
-			secondNextCallbackSpy = jasmine.createSpy('secondNext');
+		it('multiple error occurrences - only the first event should pass to the next middleware', () => {
+			const
+				error = new Error('My awesome error'),
+				firstEvent = createLogEvent(error, 'first'),
+				firstEventCopy = copyLogEvent(firstEvent),
+				secondEvent = createLogEvent(error, 'second'),
+				firstNextCallbackSpy = jasmine.createSpy('firstNext'),
+				secondNextCallbackSpy = jasmine.createSpy('secondNext');
 
-		this.middleware.exec(firstEvent, firstNextCallbackSpy);
-		this.middleware.exec(secondEvent, secondNextCallbackSpy);
+			this.middleware.exec(firstEvent, firstNextCallbackSpy);
+			this.middleware.exec(secondEvent, secondNextCallbackSpy);
 
-		expect(firstNextCallbackSpy).toHaveBeenCalledOnceWith(firstEventCopy);
-		expect(secondNextCallbackSpy).not.toHaveBeenCalled();
-	});
+			expect(firstNextCallbackSpy).toHaveBeenCalledOnceWith(firstEventCopy);
+			expect(secondNextCallbackSpy).not.toHaveBeenCalled();
+		});
 
-	it('multiple occurrences of errors with different references and the same message - all events pass to the next middleware', () => {
-		const
-			firstEvent = createLogEvent(new Error('Error'), 'first'),
-			firstEventCopy = copyLogEvent(firstEvent),
-			secondEvent = createLogEvent(new Error('Error'), 'second'),
-			secondEventCopy = copyLogEvent(secondEvent),
-			firstNextCallbackSpy = jasmine.createSpy('firstNext'),
-			secondNextCallbackSpy = jasmine.createSpy('secondNext');
+		it('multiple occurrences of errors with different references and the same message - all events pass to the next middleware', () => {
+			const
+				firstEvent = createLogEvent(new Error('Error'), 'first'),
+				firstEventCopy = copyLogEvent(firstEvent),
+				secondEvent = createLogEvent(new Error('Error'), 'second'),
+				secondEventCopy = copyLogEvent(secondEvent),
+				firstNextCallbackSpy = jasmine.createSpy('firstNext'),
+				secondNextCallbackSpy = jasmine.createSpy('secondNext');
 
-		this.middleware.exec(firstEvent, firstNextCallbackSpy);
-		this.middleware.exec(secondEvent, secondNextCallbackSpy);
+			this.middleware.exec(firstEvent, firstNextCallbackSpy);
+			this.middleware.exec(secondEvent, secondNextCallbackSpy);
 
-		expect(firstNextCallbackSpy).toHaveBeenCalledOnceWith(firstEventCopy);
-		expect(secondNextCallbackSpy).toHaveBeenCalledOnceWith(secondEventCopy);
-	});
+			expect(firstNextCallbackSpy).toHaveBeenCalledOnceWith(firstEventCopy);
+			expect(secondNextCallbackSpy).toHaveBeenCalledOnceWith(secondEventCopy);
+		});
 
-	function createLogEvent(error, context = 'test') {
-		const logEvent = {
-			context,
-			level: error ? 'error' : 'info',
-			additionals: {}
-		};
+		function createLogEvent(error, context = 'test') {
+			const logEvent = {
+				context,
+				level: error ? 'error' : 'info',
+				additionals: {}
+			};
 
-		if (error) {
-			logEvent.error = error;
+			if (error) {
+				logEvent.error = error;
+			}
+
+			return logEvent;
 		}
 
-		return logEvent;
-	}
+		function copyLogEvent(srcLogEvent) {
+			const copy = {
+				context: srcLogEvent.context,
+				level: srcLogEvent.level,
+				additionals: srcLogEvent.additionals
+			};
 
-	function copyLogEvent(srcLogEvent) {
-		const copy = {
-			context: srcLogEvent.context,
-			level: srcLogEvent.level,
-			additionals: srcLogEvent.additionals
-		};
+			if (srcLogEvent.error) {
+				copy.error = srcLogEvent.error;
+			}
 
-		if (srcLogEvent.error) {
-			copy.error = srcLogEvent.error;
+			return copy;
 		}
-
-		return copy;
-	}
+	});
 });
