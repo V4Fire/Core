26ef58b2d2bd3ceb77c4ce76780b3f4a4fe1033f
ExactlyNoSense <whois.ard@gmail.com>
2021-09-06 16:34:03 +0300

    :white_check_mark: Improved convertIfDate tests

--- src/core/json/spec.js
+++ src/core/json/spec.js
@@ -9,8 +9,40 @@
 import { convertIfDate } from 'core/json';
 
 describe('core/json', () => {
-	it('`convertIfDate`', () => {
-		expect(JSON.parse('"2015-10-12"', convertIfDate).is(new Date(2015, 9, 12))).toBeTrue();
-		expect(JSON.parse('"2015-10"', convertIfDate)).toBe('2015-10');
+	describe('`convertIfDate`', () => {
+		['-', '.'].forEach((delimiter) => {
+			describe(`with delimiter '${delimiter}'`, () => {
+				testConvertIfDateCases(['YYYY', 'MM', 'DD'], [2015, 10, 12], delimiter, new Date(2015, 9, 12));
+
+				testConvertIfDateCases(['YYYY', 'MM', 'DD'], [2021, '03', '01'], delimiter, new Date(2021, 2, 1));
+
+				testConvertIfDateCases(['YYYY', 'M', 'D'], [2021, 6, 3], delimiter, [2021, 6, 3].join(delimiter));
+			});
+		});
 	});
 });
+
+function testConvertIfDateCases(templateParts, dateParts, delimiter, expectedDate) {
+	const
+		dateTemplate = templateParts.join(delimiter),
+		dateString = dateParts.join(delimiter);
+
+	it(`string ${dateTemplate} is parsed as date`, () => {
+		expect(JSON.parse(`"${dateString}"`, convertIfDate)).toEqual(expectedDate);
+	});
+
+	it('incorrect date string is not parsed as date', () => {
+		const dateString = dateParts.slice(0, 2).join(delimiter);
+		expect(JSON.parse(`"${dateString}"`, convertIfDate)).toBe(dateString);
+	});
+
+	it(`string ${dateTemplate} with prefix is not parsed as date`, () => {
+		const str = `not-a-date-${dateString}`;
+		expect(JSON.parse(`"${str}"`, convertIfDate)).toBe(str);
+	});
+
+	it(`string ${dateTemplate} with suffix in not parsed as date`, () => {
+		const str = `${dateString}-not-a-date`;
+		expect(JSON.parse(`"${str}"`, convertIfDate)).toBe(str);
+	});
+}
