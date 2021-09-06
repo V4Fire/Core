c073fcecf24227977f675a69ec0152a4e07c1e12
ExactlyNoSense <whois.ard@gmail.com>
2021-09-06 16:31:53 +0300

    Made date regexp more strickter

--- src/core/prelude/date/const.ts
+++ src/core/prelude/date/const.ts
@@ -15,7 +15,7 @@ export const
 	zoneChunkRgxp = /(Z?([+-]\d{2}:?\d{2})?)/;
 
 export const
-	normalizeDateChunkRgxp = /(\d{1,4})[-./](\d{1,2})[-./](\d{1,4})/,
+	normalizeDateChunkRgxp = /^(\d{1,4})[-./](\d{1,2})[-./](\d{1,4})$/,
 	normalizeZoneRgxp = /^[+-]\d{4}$/;
 
 export const
