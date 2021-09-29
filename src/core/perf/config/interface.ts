ad28fb3217eb0e8d0653d748b3c6c2dcaea22d67
ExactlyNoSense <whois.ard@gmail.com>
2021-09-29 13:36:48 +0300

    Another comments updates

--- src/core/perf/config/interface.ts
+++ src/core/perf/config/interface.ts
@@ -9,21 +9,48 @@
 import type { PerfTimerEngineName } from 'core/perf/timer/engines';
 import type { PerfGroup } from 'core/perf/interface';
 
+/**
+ * General config for performance metrics
+ */
 export interface PerfConfig {
+	/**
+	 * Performance timers config
+	 */
 	timer: PerfTimerConfig;
 }
 
+/**
+ * Performance timers config
+ */
 export interface PerfTimerConfig {
+	/**
+	 * Engine name
+	 */
 	engine: PerfTimerEngineName;
+
+	/**
+	 * Filtering settings for each group
+	 */
 	filters?: PerfGroupFilters;
 }
 
+/**
+ * Filtering settings by group
+ */
 export type PerfGroupFilters = {
 	[K in PerfGroup]?: string[] | boolean;
 };
 
+/**
+ * Simple filtering predicates for each group.
+ * Produced from filtering settings.
+ * @see PerfGroupFilters
+ */
 export type PerfPredicates = {
 	[K in PerfGroup]: PerfPredicate;
 };
 
+/**
+ * Filtering predicate
+ */
 export type PerfPredicate = (ns: string) => boolean;
