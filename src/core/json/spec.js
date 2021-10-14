da751df54d1da334a6535d3d9620a50c5cb889e8
Andrey Dikarev <32389251+gretzkiy@users.noreply.github.com>
2021-10-14 12:38:45 +0300

    Fix converting ISO string to date with convertIfDate function (#233)
    * Fix converting ISO string to date with convertIfDate function
    
    * Add tests

--- src/core/json/spec.js
+++ src/core/json/spec.js
@@ -12,17 +12,40 @@ describe('core/json', () => {
 	describe('`convertIfDate`', () => {
 		['-', '.'].forEach((delimiter) => {
 			describe(`with delimiter '${delimiter}'`, () => {
-				testConvertIfDateCases(['YYYY', 'MM', 'DD'], [2015, 10, 12], delimiter, new Date(2015, 9, 12));
+				testConvertIfDateCasesByDateParts(['YYYY', 'MM', 'DD'], [2015, 10, 12], delimiter, new Date(2015, 9, 12));
 
-				testConvertIfDateCases(['YYYY', 'MM', 'DD'], [2021, '03', '01'], delimiter, new Date(2021, 2, 1));
+				testConvertIfDateCasesByDateParts(['YYYY', 'MM', 'DD'], [2021, '03', '01'], delimiter, new Date(2021, 2, 1));
 
-				testConvertIfDateCases(['YYYY', 'M', 'D'], [2021, 6, 3], delimiter, [2021, 6, 3].join(delimiter));
+				testConvertIfDateCasesByDateParts(['YYYY', 'M', 'D'], [2021, 6, 3], delimiter, [2021, 6, 3].join(delimiter));
+			});
+		});
+
+		describe('ISO string', () => {
+			const isoString = '2021-03-26T10:38:34.937604Z';
+
+			it('is parsed as a date', () => {
+				expect(JSON.parse(`"${isoString}"`, convertIfDate)).toEqual(Date.create(isoString));
+			});
+
+			it('with incorrect data is not parsed as a date', () => {
+				const incorrectIsoString = '2021-26T10:38:34.937604Z';
+				expect(JSON.parse(`"${incorrectIsoString}"`, convertIfDate)).toEqual(incorrectIsoString);
+			});
+
+			it('with prefix is not parsed as a date', () => {
+				const strWithPrefix = `not-a-date-${isoString}`;
+				expect(JSON.parse(`"${strWithPrefix}"`, convertIfDate)).toEqual(strWithPrefix);
+			});
+
+			it('with suffix is not parsed as a date', () => {
+				const strWithSuffix = `${isoString}-not-a-date`;
+				expect(JSON.parse(`"${strWithSuffix}"`, convertIfDate)).toEqual(strWithSuffix);
 			});
 		});
 	});
 });
 
-function testConvertIfDateCases(templateParts, dateParts, delimiter, expectedDate) {
+function testConvertIfDateCasesByDateParts(templateParts, dateParts, delimiter, expectedDate) {
 	const
 		dateTemplate = templateParts.join(delimiter),
 		dateString = dateParts.join(delimiter);
