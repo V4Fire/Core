fb12ee69b30dabb8cee4027fd1983e520de9ad2d
kobezzza <kobezzza@gmail.com>
2021-12-01 12:57:27 +0300

    fix: fixed TB helpers to work with TS 4.4

--- index.d.ts
+++ index.d.ts
@@ -22,37 +22,31 @@ declare namespace TB {
 
 	type Length<T extends any[]> = T['length'];
 
-	type Head<T extends any[]> = T extends [any, ...any[]] ?
-		T[0] : never;
+	type Head<T extends any[]> = T extends [infer H, ...any[]] ?
+		H : never;
 
-	type Tail<T extends any[]> = ((...t: T) => any) extends ((_: any, ...tail: infer TT) => any) ?
+	type Tail<T extends any[]> = T extends [any, ...infer TT] ?
 		TT : [];
 
-	type HasTail<T extends any[]> = T extends [any] | [] ? false : true;
+	type HasTail<T extends any[]> = Length<T> extends 0 | 1 ?
+		false : true;
 
-	type Last<T extends any[]> = {
-		0: Last<Tail<T>>;
-		1: Head<T>;
-	}[HasTail<T> extends true ? 0 : 1];
+	type Last<T extends any[]> = HasTail<T> extends true ?
+		Last<Tail<T>> : Head<T>;
 
-	type Prepend<E, T extends any[]> =
-		((head: E, ...args: T) => any) extends ((...args: infer U) => any) ? U : T;
+	type Prepend<E, T extends any[]> = [E, ...T];
 
-	type Drop<N extends number, T extends any[], I extends any[] = []> = {
-		0: Drop<N, Tail<T>, Prepend<any, I>>;
-		1: T;
-	}[Length<I> extends N ? 1 : 0];
+	type Drop<N extends number, T extends any[], I extends any[] = []> = Length<I> extends N ?
+		T : Drop<N, Tail<T>, Prepend<any, I>>;
 
 	type Pos<I extends any[]> = Length<I>;
+
 	type Next<I extends any[]> = Prepend<any, I>;
 	type Prev<I extends any[]> = Tail<I>;
 
-	type Reverse<T extends any[], R extends any[] = [], I extends any[] = []> = {
-		0: Reverse<T, Prepend<T[Pos<I>], R>, Next<I>>;
-		1: R;
-	}[Pos<I> extends Length<T> ? 1 : 0];
+	type Reverse<T extends any[], R extends any[] = [], I extends any[] = []> = Pos<I> extends Length<T> ?
+		R : Reverse<T, Prepend<T[Pos<I>], R>, Next<I>>;
 
-	// @ts-ignore (recursive type)
 	type Concat<T1 extends any[], T2 extends any[]> = Reverse<Cast<Reverse<T1>, any[]>, T2>;
 
 	type Append<E, T extends any[]> = Concat<T, [E]>;
@@ -62,12 +56,10 @@ declare namespace TB {
 	type GapOf<T1 extends any[], T2 extends any[], TN extends any[], I extends any[]> =
 		T1[Pos<I>] extends __ ? Append<T2[Pos<I>], TN> : TN;
 
-	type GapsOf<T1 extends any[], T2 extends any[], TN extends any[] = [], I extends any[] = []> = {
-		// @ts-ignore (recursive type)
-		0: GapsOf<T1, T2, Cast<GapOf<T1, T2, TN, I>, any[]>, Next<I>>;
-		// @ts-ignore (recursive type)
-		1: Concat<TN, Cast<Drop<Pos<I>, T2>, any[]>>;
-	}[Pos<I> extends Length<T1> ? 1 : 0];
+	type GapsOf<T1 extends any[], T2 extends any[], TN extends any[] = [], I extends any[] = []> =
+		Pos<I> extends Length<T1> ?
+			Concat<TN, Cast<Drop<Pos<I>, T2>, any[]>> :
+			GapsOf<T1, T2, Cast<GapOf<T1, T2, TN, I>, any[]>, Next<I>>;
 
 	type PartialGaps<T extends any[]> = {
 		[K in keyof T]?: T[K] | __;
@@ -77,12 +69,12 @@ declare namespace TB {
 		[K in keyof T]: NonNullable<T[K]>;
 	};
 
-	type Gaps<T extends any[]> = CleanedGaps<PartialGaps<T>>;
+	type Gaps<T extends any[]> = CleanedGaps<PartialGaps<T>> extends [...infer A] ?
+		A : never;
 
 	type Curry<F extends ((...args: any) => any)> =
-		<T extends any[]>(...args: Cast<Cast<T, Gaps<Parameters<F>>>, any[]>) =>
+		<T extends any[]>(...args: Cast<T, Gaps<Parameters<F>>>) =>
 			GapsOf<T, Parameters<F>> extends [any, ...any[]] ?
-				// @ts-ignore (recursive type)
 				Curry<(...args: Cast<GapsOf<T, Parameters<F>>, any[]>) => ReturnType<F>> :
 				ReturnType<F>;
 }
