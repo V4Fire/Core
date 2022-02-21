dc9d5d5436fd185fdb2a0baada4e94dd194c8824
kobezzza <kobezzza@gmail.com>
2022-02-21 19:27:12 +0300

    refactor: review specs

--- src/core/json/stream/spec.js
+++ src/core/json/stream/spec.js
@@ -6,134 +6,185 @@
  * https://github.com/V4Fire/Core/blob/master/LICENSE
  */
 
-import { from, assemble, filter, pick, streamArray } from 'core/json/stream';
-
-const data = [
-	'[{\n\t"fields": "利点がある,利点はない,不詳",\n\t"data": [\n\t\t{\n\t\t\t"count": 314,\n\t\t\t"field": "利点がある",\n\t\t\t"age":',
-	' "18-19",\n\t\t\t"investigation-year": 1987,\n\t\t\t"sex": "M"\n\t\t},\n\t\t{\n\t\t\t"count": 966,\n\t\t\t"field": "利点がある",\n\t\t\t"age": "20-24',
-	'",\n\t\t\t"investigation-year": 1987,\n\t\t\t"sex": "M"\n\t\t},\n\t\t{\n\t\t\t"count": 680,\n\t\t\t"field": "利点がある",\n\t\t\t"age": "25-29",\n\t\t\t"',
-	'investigation-year": 1987,\n\t\t\t"sex": "M"\n\t\t},\n\t\t{\n\t\t\t"count": 318,\n\t\t\t"field": "利点がある",\n\t\t\t"age": "30-34",\n\t\t\t"investi',
-	'gation-year": 1987,\n\t\t\t"sex": "M"\n\t\t}\n\t],\n\t"id": "S007",\n\t"name": "調査別、男女年齢（５歳階級）別、結婚の利�',
-	'��の有無および独身生活の利点の有無別、未婚者数"\n},\n{\n\t"conformsTo": "https://project-open-data.cio.gov/v1.',
-	'1/schema",\n\t"describedBy": "https://project-open-data.cio.gov/v1.1/schema/catalog.json",\n\t"@context": "https://project-open-data',
-	'.cio.gov/v1.1/schema/catalog.jsonld",\n\t"@type": "dcat:Catalog"\n},\n{\n\t"фальшивые данные чтобы добавить ',
-	'разных типов": [1, null, true, false, "текст на русском"]\n}]\n'
-];
-
-const target = [
-	{
-		fields: '利点がある,利点はない,不詳',
-		data: [
-			{
-				count: 314,
-				field: '利点がある',
-				age: '18-19',
-				'investigation-year': 1987,
-				sex: 'M'
-			},
-			{
-				count: 966,
-				field: '利点がある',
-				age: '20-24',
-				'investigation-year': 1987,
-				sex: 'M'
-			},
-			{
-				count: 680,
-				field: '利点がある',
-				age: '25-29',
-				'investigation-year': 1987,
-				sex: 'M'
-			},
-			{
-				count: 318,
-				field: '利点がある',
-				age: '30-34',
-				'investigation-year': 1987,
-				sex: 'M'
-			}
-		],
-		id: 'S007',
-		name: '調査別、男女年齢（５歳階級）別、結婚の利���の有無および独身生活の利点の有無別、未婚者数'
-	},
-	{
-		conformsTo: 'https://project-open-data.cio.gov/v1.1/schema',
-		describedBy: 'https://project-open-data.cio.gov/v1.1/schema/catalog.json',
-		'@context': 'https://project-open-data.cio.gov/v1.1/schema/catalog.jsonld',
-		'@type': 'dcat:Catalog'
-	},
-	{
-		'фальшивые данные чтобы добавить разных типов': [1, null, true, false, 'текст на русском']
-	}
-];
-
-const asyncIterable = {
-	async*[Symbol.asyncIterator]() {
-		let i = 0;
-
-		while (i < data.length) {
-			await new Promise((resolve) => setTimeout(resolve, 10));
-			yield data[i++];
-		}
-	}
-};
+import { from, filter, pick, assemble, streamArray, streamObject } from 'core/json/stream';
 
 describe('core/json/stream', () => {
-	describe('json stream parser', () => {
-		it('should parse json from async iterator to tokens', async () => {
-			const result = [];
+	describe('`from`', () => {
+		it('should parse sync JSON stream to tokens', async () => {
+			const
+				tokens = [];
 
-			for await (const chunk of from(asyncIterable)) {
-				result.push(chunk);
+			for await (const token of from(['[1, {"a": 1}, true]'])) {
+				tokens.push(token);
 			}
 
-			expect(result.length).toBe(268);
+			expect(tokens).toEqual([
+				{name: 'startArray'},
+				{name: 'startNumber'},
+				{name: 'numberChunk', value: '1'},
+				{name: 'endNumber'},
+				{name: 'numberValue', value: '1'},
+				{name: 'startObject'},
+				{name: 'startKey'},
+				{name: 'stringChunk', value: 'a'},
+				{name: 'endKey'},
+				{name: 'keyValue', value: 'a'},
+				{name: 'startNumber'},
+				{name: 'numberChunk', value: '1'},
+				{name: 'endNumber'},
+				{name: 'numberValue', value: '1'},
+				{name: 'endObject'},
+				{name: 'trueValue', value: true},
+				{name: 'endArray'}
+			]);
 		});
 
+		it('should parse async JSON stream to tokens', async () => {
+			const
+				tokens = [];
+
+			for await (const token of from(intoAsyncIter(['[1, {"a', '": 1},', 'true]']))) {
+				tokens.push(token);
+			}
+
+			expect(tokens).toEqual([
+				{name: 'startArray'},
+				{name: 'startNumber'},
+				{name: 'numberChunk', value: '1'},
+				{name: 'endNumber'},
+				{name: 'numberValue', value: '1'},
+				{name: 'startObject'},
+				{name: 'startKey'},
+				{name: 'stringChunk', value: 'a'},
+				{name: 'endKey'},
+				{name: 'keyValue', value: 'a'},
+				{name: 'startNumber'},
+				{name: 'numberChunk', value: '1'},
+				{name: 'endNumber'},
+				{name: 'numberValue', value: '1'},
+				{name: 'endObject'},
+				{name: 'trueValue', value: true},
+				{name: 'endArray'}
+			]);
+		});
 	});
 
-	describe('json stream assembler', () => {
-		it('should assemble valid js structure from json tokens', async () => {
-			let result;
+	describe('filters', () => {
+		it('should filter JSON tokens and preserves only that matched to a filter', async () => {
+			const
+				tokens = [];
 
-			for await (const chunk of assemble(from(asyncIterable))) {
-				result = chunk;
+			for await (const token of filter(from(['{"a": 1, "b": 2, "data": [1, 2, 3]}']), /data/)) {
+				tokens.push(token);
 			}
 
-			expect(result).toEqual(target);
+			expect(tokens).toEqual([
+				{name: 'startObject'},
+				{name: 'startKey'},
+				{name: 'stringChunk', value: 'data'},
+				{name: 'endKey'},
+				{name: 'keyValue', value: 'data'},
+				{name: 'startArray'},
+				{name: 'startNumber'},
+				{name: 'numberChunk', value: '1'},
+				{name: 'endNumber'},
+				{name: 'numberValue', value: '1'},
+				{name: 'startNumber'},
+				{name: 'numberChunk', value: '2'},
+				{name: 'endNumber'},
+				{name: 'numberValue', value: '2'},
+				{name: 'startNumber'},
+				{name: 'numberChunk', value: '3'},
+				{name: 'endNumber'},
+				{name: 'numberValue', value: '3'},
+				{name: 'endArray'},
+				{name: 'endObject'}
+			]);
 		});
 
+		it('should pick tokens from a stream by the specified selector', async () => {
+			const
+				tokens = [];
+
+			for await (const token of pick(from('{"a": 1, "b": 2, "data": [1, 2, 3]}'), /data/)) {
+				tokens.push(token);
+			}
+
+			expect(tokens).toEqual([
+				{name: 'startArray'},
+				{name: 'startNumber'},
+				{name: 'numberChunk', value: '1'},
+				{name: 'endNumber'},
+				{name: 'numberValue', value: '1'},
+				{name: 'startNumber'},
+				{name: 'numberChunk', value: '2'},
+				{name: 'endNumber'},
+				{name: 'numberValue', value: '2'},
+				{name: 'startNumber'},
+				{name: 'numberChunk', value: '3'},
+				{name: 'endNumber'},
+				{name: 'numberValue', value: '3'},
+				{name: 'endArray'}
+			]);
+		});
 	});
 
-	describe('json stream filters', () => {
-		it('should filter json and take only data field with structure saving', async () => {
-			let result;
+	describe('`assemble`', () => {
+		it('should assemble a valid js structure from the given json tokens', async () => {
+			const data = [
+				{a: 1},
+				[2, 3, 4],
+				true
+			];
 
-			for await (const chunk of assemble(filter(from(asyncIterable), {filter: /data/}))) {
-				result = chunk;
+			let
+				res;
+
+			for await (const val of assemble(from(intoAsyncIter(JSON.stringify(data))))) {
+				res = val;
 			}
 
-			expect(result).toEqual([{data: target[0].data}]);
-		});
-
-		it('should filter json and take only data field without structure saving', async () => {
-			let result;
-
-			for await (const chunk of assemble(pick(from(asyncIterable), {filter: /data/}))) {
-				result = chunk;
-			}
-
-			expect(result).toEqual(target[0].data);
+			expect(res).toEqual(data);
 		});
 	});
 
-	describe('json stream streams', () => {
-		it('should stream array element in every iteration', async () => {
-			let index = 0;
+	describe('streamers', () => {
+		it('should stream array elements from JSON tokens', async () => {
+			const
+				elements = [];
 
-			for await (const chunk of streamArray(pick(from(asyncIterable), {filter: /data/}))) {
-				expect(chunk).toEqual({key: index, value: target[0].data[index++]});
+			for await (const el of streamArray(from('[{"a": 1}, true, 0.1e12]'))) {
+				elements.push(el);
 			}
+
+			expect(elements).toEqual([
+				{index: 0, value: {a: 1}},
+				{index: 1, value: true},
+				{index: 2, value: 100000000000}
+			]);
+		});
+
+		it('should stream object elements from JSON tokens', async () => {
+			const
+				elements = [];
+
+			for await (const el of streamObject(from('{"a": 1, "b": [1, 2], "c": true, "d": {"a": 1}'))) {
+				elements.push(el);
+			}
+
+			expect(elements).toEqual([
+				{key: 'a', value: 1},
+				{key: 'b', value: [1, 2]},
+				{key: 'c', value: true},
+				{key: 'd', value: {a: 1}}
+			]);
 		});
 	});
 });
+
+async function* intoAsyncIter(source) {
+	for (const val of source) {
+		await new Promise((resolve) => setTimeout(resolve, 10));
+		yield val;
+	}
+}
