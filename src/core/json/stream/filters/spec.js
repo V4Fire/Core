5e6829c8e11b0aa77753e846e4849aa6f6fec16f
kobezzza <kobezzza@gmail.com>
2022-02-22 18:24:48 +0300

    fix: fixed a spec

--- src/core/json/stream/filters/spec.js
+++ src/core/json/stream/filters/spec.js
@@ -7,7 +7,6 @@
  */
 
 import Parser from 'core/json/stream/parser';
-import Assembler from 'core/json/stream/assembler';
 import { Filter, Pick } from 'core/json/stream/filters';
 
 describe('core/json/stream/filters', () => {
@@ -24,16 +23,9 @@ describe('core/json/stream/filters', () => {
 			const
 				tokens = [];
 
-			const f = (path, token) => {
-				return path.includes('a') && token.name === 'numberValue';
-			};
-
-			for await (const token of Parser.from(['{"total": 2, "data": {"a": [1, true, 2], "b": [2, 3]}}'], new Pick(f, {multiple: true}), new Assembler())) {
-				console.log(11, token);
+			for await (const token of Parser.from([input], new Filter('a.b'))) {
+				tokens.push(token);
 			}
-
-			//console.log(tokens);
-			//console.dir(tokens, {depth: null});
 
 			const expected = [
 				...new Parser().processChunk(`{
@@ -43,7 +35,7 @@ describe('core/json/stream/filters', () => {
 				}`)
 			];
 
-			//expect(tokens).toEqual(expected);
+			expect(tokens).toEqual(expected);
 		});
 
 		it('filtering tokens by the specified RegExp', async () => {
