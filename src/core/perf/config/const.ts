35d7258a7f6556e8e86840e167e0a3e55e7e9736
kz2d <avidex111@gmail.com>
2022-03-21 13:38:17 +0300

    implemented

---
+++ src/core/perf/config/const.ts
@@ -0,0 +1,12 @@
+/*!
+ * V4Fire Core
+ * https://github.com/V4Fire/Core
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Core/blob/master/LICENSE
+ */
+
+/**
+ * Mark array of regexp if should events should be excluded
+ */
+export const EXCLUDE = Symbol('is pattern excluded');
