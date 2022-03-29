febea190ed5ea93203208629de88e486a4084afb
kz2d <avidex111@gmail.com>
2022-03-29 20:20:39 +0300

    added README.md and spec.js

---
+++ src/core/perf/config/spec.js
@@ -0,0 +1,122 @@
+/*!
+ * V4Fire Core
+ * https://github.com/V4Fire/Core
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Core/blob/master/LICENSE
+ */
+import { perf as perfFactory } from 'core/perf';
+
+describe('core/perf', () => {
+	it('old pattern', () => {
+		const spy = spyOn(console, 'warn');
+
+		const perf = perfFactory({
+				timer: {
+					engine: 'console',
+					filters: {
+						network: ['start']
+					}
+				}
+		});
+
+		const timer = perf.getTimer('network').namespace('auth');
+
+		const timerId = timer.start('start');
+		timer.finish(timerId);
+
+		expect(spy).toHaveBeenCalledTimes(1);
+
+		const timerId2 = timer.start('end');
+		timer.finish(timerId2);
+
+		expect(spy).toHaveBeenCalledTimes(1);
+	});
+
+	it('include pattern', () => {
+		const spy = spyOn(console, 'warn');
+
+		const perf = perfFactory({
+			timer: {
+				engine: 'console',
+				filters: {
+					components: {
+						include: ['start']
+					}
+				}
+			}
+		});
+
+		const timer = perf.getTimer('components').namespace('auth');
+
+		const timerId = timer.start('start');
+		timer.finish(timerId);
+
+		expect(spy).toHaveBeenCalledTimes(1);
+
+		const timerId2 = timer.start('end');
+		timer.finish(timerId2);
+
+		expect(spy).toHaveBeenCalledTimes(1);
+	});
+
+	it('exclude pattern', () => {
+		const spy = spyOn(console, 'warn');
+
+		const perf = perfFactory({
+			timer: {
+				engine: 'console',
+				filters: {
+					tools: {
+						exclude: ['start']
+					}
+				}
+			}
+		});
+
+		const timer = perf.getTimer('tools').namespace('auth');
+
+		const timerId = timer.start('start');
+		timer.finish(timerId);
+
+		expect(spy).toHaveBeenCalledTimes(0);
+
+		const timerId2 = timer.start('end');
+		timer.finish(timerId2);
+
+		expect(spy).toHaveBeenCalledTimes(1);
+
+		const timerId3 = timer.start('intermediate');
+		timer.finish(timerId3);
+
+		expect(spy).toHaveBeenCalledTimes(2);
+	});
+
+	it('include and exclude, exclude is ignored', () => {
+		const spy = spyOn(console, 'warn');
+
+		const perf = perfFactory({
+			timer: {
+				engine: 'console',
+				filters: {
+					manual: {
+						include: ['end', 'start'],
+						exclude: ['start']
+					}
+				}
+			}
+		});
+
+		const timer = perf.getTimer('manual').namespace('auth');
+
+		const timerId = timer.start('start');
+		timer.finish(timerId);
+
+		expect(spy).toHaveBeenCalledTimes(1);
+
+		const timerId2 = timer.start('end');
+		timer.finish(timerId2);
+
+		expect(spy).toHaveBeenCalledTimes(2);
+	});
+});
