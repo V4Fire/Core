380d6c41c6f8b018ac96298f75b7d419b303f672
kz2d <avidex111@gmail.com>
2022-03-29 20:28:38 +0300

    refactored spec.js

--- src/core/perf/config/spec.js
+++ src/core/perf/config/spec.js
@@ -5,19 +5,19 @@
  * Released under the MIT license
  * https://github.com/V4Fire/Core/blob/master/LICENSE
  */
-import { perf as perfFactory } from 'core/perf';
+import {perf as perfFactory} from 'core/perf';
 
 describe('core/perf', () => {
 	it('old pattern', () => {
 		const spy = spyOn(console, 'warn');
 
 		const perf = perfFactory({
-				timer: {
-					engine: 'console',
-					filters: {
-						network: ['start']
-					}
+			timer: {
+				engine: 'console',
+				filters: {
+					network: ['start']
 				}
+			}
 		});
 
 		const timer = perf.getTimer('network').namespace('auth');
@@ -47,7 +47,7 @@ describe('core/perf', () => {
 			}
 		});
 
-		const timer = perf.getTimer('components').namespace('auth');
+		const timer = perf.getTimer('components').namespace('render');
 
 		const timerId = timer.start('start');
 		timer.finish(timerId);
@@ -74,7 +74,7 @@ describe('core/perf', () => {
 			}
 		});
 
-		const timer = perf.getTimer('tools').namespace('auth');
+		const timer = perf.getTimer('tools').namespace('anything');
 
 		const timerId = timer.start('start');
 		timer.finish(timerId);
@@ -107,7 +107,7 @@ describe('core/perf', () => {
 			}
 		});
 
-		const timer = perf.getTimer('manual').namespace('auth');
+		const timer = perf.getTimer('manual').namespace('click');
 
 		const timerId = timer.start('start');
 		timer.finish(timerId);
