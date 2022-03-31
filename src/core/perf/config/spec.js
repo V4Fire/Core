7be0207c14b5132735574b0e93381479a0cfb2c3
kobezzza <kobezzza@gmail.com>
2022-03-31 16:34:41 +0300

    chore: better naming

--- src/core/perf/config/spec.js
+++ src/core/perf/config/spec.js
@@ -5,10 +5,33 @@
  * Released under the MIT license
  * https://github.com/V4Fire/Core/blob/master/LICENSE
  */
+
 import { perf as perfFactory } from 'core/perf';
 
 describe('core/perf', () => {
-	it('old pattern', () => {
+	it('providing an engine to use', () => {
+		const spy = spyOn(console, 'warn');
+
+		const perf = perfFactory({
+			timer: {
+				engine: 'console'
+			}
+		});
+
+		const timer = perf.getTimer('network').namespace('auth');
+
+		const timerId = timer.start('start');
+		timer.finish(timerId);
+
+		expect(spy).toHaveBeenCalledTimes(1);
+
+		const timerId2 = timer.start('end');
+		timer.finish(timerId2);
+
+		expect(spy).toHaveBeenCalledTimes(2);
+	});
+
+	it('should log only filtered events', () => {
 		const spy = spyOn(console, 'warn');
 
 		const perf = perfFactory({
@@ -33,7 +56,54 @@ describe('core/perf', () => {
 		expect(spy).toHaveBeenCalledTimes(1);
 	});
 
-	it('include pattern', () => {
+	it('should filter namespaces too', () => {
+		const spy = spyOn(console, 'warn');
+
+		const perf = perfFactory({
+			timer: {
+				engine: 'console',
+				filters: {
+					network: ['start']
+				}
+			}
+		});
+
+		const timer = perf.getTimer('network').namespace('start');
+
+		const timerId = timer.start('anything');
+		timer.finish(timerId);
+
+		expect(spy).toHaveBeenCalledTimes(1);
+
+		const timer2 = perf.getTimer('network').namespace('finish');
+
+		const timerId2 = timer2.start('anything');
+		timer2.finish(timerId2);
+
+		expect(spy).toHaveBeenCalledTimes(1);
+	});
+
+	it('should filter only `manual`', () => {
+		const spy = spyOn(console, 'warn');
+
+		const perf = perfFactory({
+			timer: {
+				engine: 'console',
+				filters: {
+					manual: ['start']
+				}
+			}
+		});
+
+		const timer = perf.getTimer('network').namespace('click');
+
+		const timerId = timer.start('start');
+		timer.finish(timerId);
+
+		expect(spy).toHaveBeenCalledTimes(1);
+	});
+
+	it('should log only filtered events provided via `include`', () => {
 		const spy = spyOn(console, 'warn');
 
 		const perf = perfFactory({
@@ -65,7 +135,7 @@ describe('core/perf', () => {
 		expect(spy).toHaveBeenCalledTimes(2);
 	});
 
-	it('exclude pattern', () => {
+	it("shouldn't log events provided via `exclude`", () => {
 		const spy = spyOn(console, 'warn');
 
 		const perf = perfFactory({
@@ -97,7 +167,7 @@ describe('core/perf', () => {
 		expect(spy).toHaveBeenCalledTimes(1);
 	});
 
-	it('include and exclude, exclude is ignored', () => {
+	it('should ignore `exclude` if provided `include`', () => {
 		const spy = spyOn(console, 'warn');
 
 		const perf = perfFactory({
@@ -128,56 +198,5 @@ describe('core/perf', () => {
 		timer.finish(timerId3);
 
 		expect(spy).toHaveBeenCalledTimes(2);
-	});
-
-	it('filters also applied to namespaces', () => {
-		const spy = spyOn(console, 'warn');
-
-		const perf = perfFactory({
-			timer: {
-				engine: 'console',
-				filters: {
-					network: {
-						include: ['start']
-					}
-				}
-			}
-		});
-
-		const timer = perf.getTimer('network').namespace('start');
-
-		const timerId = timer.start('anything');
-		timer.finish(timerId);
-
-		expect(spy).toHaveBeenCalledTimes(1);
-
-		const timer2 = perf.getTimer('network').namespace('finish');
-
-		const timerId2 = timer2.start('anything');
-		timer2.finish(timerId2);
-
-		expect(spy).toHaveBeenCalledTimes(1);
-	});
-
-	it('filters apply only to specific group', () => {
-		const spy = spyOn(console, 'warn');
-
-		const perf = perfFactory({
-			timer: {
-				engine: 'console',
-				filters: {
-					manual: {
-						exclude: ['start']
-					}
-				}
-			}
-		});
-
-		const timer = perf.getTimer('network').namespace('click');
-
-		const timerId = timer.start('start');
-		timer.finish(timerId);
-
-		expect(spy).toHaveBeenCalledTimes(1);
 	});
 });
