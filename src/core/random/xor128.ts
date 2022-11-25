8da6439c8e154421998f891a47d607059ed27a3e
kobezzza <kobezzza@gmail.com>
2022-11-25 16:29:15 +0300

    feat: added a new module `core/random`

---
+++ src/core/random/xor128.ts
@@ -0,0 +1,48 @@
+/* eslint-disable no-bitwise */
+
+/*!
+ * V4Fire Core
+ * https://github.com/V4Fire/Core
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Core/blob/master/LICENSE
+ */
+
+export class Xor128 implements IterableIterator<number> {
+	protected x: number;
+
+	protected y: number = 0;
+	protected z: number = 0;
+	protected w: number = 0;
+
+	constructor(seed: number) {
+		this.x = seed;
+
+		for (let i = 0; i < 64; i++) {
+			this.next();
+		}
+	}
+
+	[Symbol.iterator](): IterableIterator<number> {
+		return this;
+	}
+
+	next(): IteratorResult<number> {
+		const
+			t = this.x ^ (this.x << 11);
+
+		this.x = this.y;
+		this.y = this.z;
+		this.z = this.w;
+		this.w ^= (this.w >> 19) ^ t ^ (t >> 8);
+
+		return {
+			done: false,
+			value: this.w
+		};
+	}
+
+	random(): number {
+		return (this.next().value >>> 0) / ((1 << 30) * 4);
+	}
+}
