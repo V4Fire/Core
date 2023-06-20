c7d646aca8bb545412d3c2a1949532cde89cfb42
Dmitrii <lexur-02@yandex.ru>
2023-06-20 18:45:33 +0500

    added string kv-storage engine

---
+++ src/core/kv-storage/engines/string/index.ts
@@ -0,0 +1,17 @@
+/*!
+ * V4Fire Core
+ * https://github.com/V4Fire/Core
+ *
+ * Released under the MIT license
+ * https://github.com/V4Fire/Core/blob/master/LICENSE
+ */
+
+import StringEngine from 'core/kv-storage/engines/string/engine';
+
+export const
+	syncSessionStorage = new StringEngine(),
+	asyncSessionStorage = syncSessionStorage;
+
+export const
+	syncLocalStorage = new StringEngine(),
+	asyncLocalStorage = syncLocalStorage;
