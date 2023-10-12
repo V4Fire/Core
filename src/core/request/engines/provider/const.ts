39aec4ef43b621aaa92838a075f64ad38985443d
kobezzza <kobezzza@gmail.com>
2023-10-12 16:27:46 +0300

    chore: fixed linters

--- src/core/request/engines/provider/const.ts
+++ src/core/request/engines/provider/const.ts
@@ -6,9 +6,7 @@
  * https://github.com/V4Fire/Core/blob/master/LICENSE
  */
 
-export const deniedProviderParams = [
-	'socket'
-];
+export const deniedProviderParams = ['socket'];
 
 export const availableParams = [
 	'url',
