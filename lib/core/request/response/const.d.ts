2cc536c549a8566390b4fc7452bad4f611aa5398
Dzmitry Yahorau <d-yahorau@yandex-team.ru>
2024-03-29 17:49:11 +0300

    rebuild /lib

--- lib/core/request/response/const.d.ts
+++ lib/core/request/response/const.d.ts
@@ -12,7 +12,7 @@ export declare const defaultResponseOpts: {
     redirected: boolean;
     status: number;
     statusText: string;
-    okStatuses: Range<import("../../range").RangeValue>;
+    okStatuses: Range<import("../../../core/range").RangeValue>;
     responseType: ResponseType;
     headers: {};
 };
