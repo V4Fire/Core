a335238571f7bfe23d36f165a869d4d9bcec2843
Dzmitry Yahorau <48996979+DmitryYegorov@users.noreply.github.com>
2024-04-03 18:05:05 +0300

    fixed unit-tests (#401)
    Co-authored-by: Dzmitry Yahorau <d-yahorau@yandex-team.ru>

--- src/core/request/index.ts
+++ src/core/request/index.ts
@@ -312,7 +312,7 @@ function request<D = unknown>(
 
 					return req.catch((err) => {
 						if (err instanceof RequestError) {
-							Object.assign(err.details, errDetails);
+							Object.assign(err.details.deref() ?? {}, errDetails);
 						}
 
 						return Promise.reject(err);
