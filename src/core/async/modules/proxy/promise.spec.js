f073ef25ff3dcc878ebb5d59a12d6b20356e9754
kobezzza <kobezzza@gmail.com>
2024-06-03 16:10:46 +0300

    chore: added more tests

--- src/core/async/modules/proxy/promise.spec.js
+++ src/core/async/modules/proxy/promise.spec.js
@@ -76,6 +76,26 @@ describe('core/async/modules/proxy `promise`', () => {
 		expect(i).toEqual(0);
 	});
 
+	it('clearing promises via clearAll', async () => {
+		let i = 0;
+
+		$a.promise(new Promise((resolve) => setTimeout(resolve, 50)));
+
+		$a.sleep(50).next(() => {
+			i++;
+		});
+
+		$a.nextTick().next(() => {
+			i++;
+		});
+
+		$a.clearAll();
+
+		await $a.sleep(100);
+
+		expect(i).toEqual(0);
+	});
+
 	describe('suspendPromise/unsuspendPromise`', () => {
 		it('suspending a promise by id', async () => {
 			let i = 0;
