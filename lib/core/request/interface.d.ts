8bc3c4a86b4d43cd47b8d1251d646ef009cae786
bonkalol <bonkalol@list.ru>
2024-06-28 14:43:05 +0300

    rollback lib

--- lib/core/request/interface.d.ts
+++ lib/core/request/interface.d.ts
@@ -87,10 +87,6 @@ export interface RequestResponseObject<D = unknown> {
      * Destroys the request context
      */
     destroy(): void;
-    /**
-     * If true, it means the object has been destroyed
-     */
-    destroyed: boolean;
 }
 export declare type RequestResponse<D = unknown> = AbortablePromise<RequestResponseObject<D>>;
 export interface RequestPromise<D = unknown> extends RequestResponse<D> {
